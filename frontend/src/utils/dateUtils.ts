4af56beed3d2738b083c08c36c68277ddb38dc7a
Satyam Thakur <sat.thakur20@gmail.com>
2025-01-11 18:45:25 +0530

    Add year in formatDate utils

--- frontend/src/utils/dateUtils.ts
+++ frontend/src/utils/dateUtils.ts
@@ -2,5 +2,6 @@ export const formatDate = (timestamp: number): string => {
   return new Date(timestamp).toLocaleDateString('en-GB', {
     day: '2-digit',
     month: 'short',
+    year: 'numeric',
   });
 };
